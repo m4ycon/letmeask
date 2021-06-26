57fc59e44bd30f1fd3698598faacd2227f9151cd
Maycon Fabio <m.vinnicyus@gmail.com>
2021-06-26 20:28:07 -0300

    Admin page done

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,6 +2,8 @@ import { useHistory, useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
 import deleteImg from '../assets/images/delete.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
 
 import { Button } from './../components/Button'
 import { RoomCode } from './../components/RoomCode'
@@ -29,6 +31,18 @@ export const AdminRoom = () => {
 		})
 
 		history.push('/')
+	}
+
+	const handleCheckQuestionAsAnswered = async (questionId: string) => {
+		await database.ref(`/rooms/${roomId}/questions/${questionId}`).update({
+			isAnswered: true,
+		})
+	}
+
+	const handleHighlightQuestion = async (questionId: string) => {
+		await database.ref(`/rooms/${roomId}/questions/${questionId}`).update({
+			isHighlighted: true,
+		})
 	}
 
 	const handleDeleteQuestion = async (questionId: string) => {
@@ -64,7 +78,25 @@ export const AdminRoom = () => {
 							key={question.id}
 							content={question.content}
 							author={question.author}
+							isHighlighted={question.isHighlighted}
+							isAnswered={question.isAnswered}
 						>
+							{!question.isAnswered && (
+								<>
+									<button
+										type='button'
+										onClick={() => handleCheckQuestionAsAnswered(question.id)}
+									>
+										<img src={checkImg} alt='Marcar pergunta como respondida' />
+									</button>
+									<button
+										type='button'
+										onClick={() => handleHighlightQuestion(question.id)}
+									>
+										<img src={answerImg} alt='Dar destaque à pergunta' />
+									</button>
+								</>
+							)}
 							<button
 								type='button'
 								onClick={() => handleDeleteQuestion(question.id)}
