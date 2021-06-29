a64fe7d67cda19263e1582c6f8828cfadd1a6060
Maycon Fabio <m.vinnicyus@gmail.com>
2021-06-29 16:17:41 -0300

    Added gray and danger styles on Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,28 @@
 import { ButtonHTMLAttributes } from 'react'
+import cx from 'classnames'
 
 import './styles.scss'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 	isOutlined?: boolean
+	isDanger?: boolean
+	isGray?: boolean
 }
 
-export const Button = ({ isOutlined = false, ...props }: ButtonProps) => {
+export const Button = ({
+	isOutlined,
+	isDanger,
+	isGray,
+	...props
+}: ButtonProps) => {
 	return (
-		<button className={`button ${isOutlined ? 'outlined' : ''}`} {...props} />
+		<button
+			className={cx('button', {
+				outlined: isOutlined,
+				danger: isDanger,
+				gray: isGray,
+			})}
+			{...props}
+		/>
 	)
 }
