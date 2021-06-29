00e70a249361a21ea6a5d58373423a95b0315102
Maycon Fabio <m.vinnicyus@gmail.com>
2021-06-29 17:09:38 -0300

    Added modal on delete operation

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -5,7 +5,6 @@ import logoImg from '../../assets/images/logo.svg'
 import deleteImg from '../../assets/images/delete.svg'
 import checkImg from '../../assets/images/check.svg'
 import answerImg from '../../assets/images/answer.svg'
-import closeImg from '../../assets/images/close.svg'
 
 import { Button } from '../../components/Button'
 import { RoomCode } from '../../components/RoomCode'
@@ -29,6 +28,8 @@ export const AdminRoom = () => {
 	const { id: roomId } = useParams<RoomParams>()
 	const { authorId, title, questions } = useRoom(roomId)
 	const [showModal, setShowModal] = useState(false)
+	const [questionIdToBeDeleted, setQuestionIdToBeDeleted] = useState('')
+	const [showModalDelete, setShowModalDelete] = useState(false)
 
 	useEffect(() => {
 		if (
@@ -66,10 +67,18 @@ export const AdminRoom = () => {
 		})
 	}
 
-	const handleDeleteQuestion = async (questionId: string) => {
-		if (window.confirm('Tem certeza que deseja excluir esta pergunta?')) {
-			await database.ref(`/rooms/${roomId}/questions/${questionId}`).remove()
-		}
+	// Controla se o modal de apagar irá aparecer,
+	// somente se houver algum id no 'questionToBeDeleted'
+	useEffect(
+		() => setShowModalDelete(questionIdToBeDeleted ? true : false),
+		[questionIdToBeDeleted]
+	)
+
+	const handleDeleteQuestion = async () => {
+		await database
+			.ref(`/rooms/${roomId}/questions/${questionIdToBeDeleted}`)
+			.remove()
+		setQuestionIdToBeDeleted('')
 	}
 
 	return (
@@ -126,7 +135,7 @@ export const AdminRoom = () => {
 							)}
 							<button
 								type='button'
-								onClick={() => handleDeleteQuestion(question.id)}
+								onClick={() => setQuestionIdToBeDeleted(question.id)}
 							>
 								<img src={deleteImg} alt='Remover pergunta' />
 							</button>
@@ -137,24 +146,58 @@ export const AdminRoom = () => {
 
 			<Modal
 				contentLabel='Encerrar sala'
+				isOpen={showModalDelete}
+				onRequestClose={() => setQuestionIdToBeDeleted('')}
+			>
+				<svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+					<path
+						d='M3 5.99988H5H21'
+						stroke='#E73F5D'
+						strokeWidth='2'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					/>
+					<path
+						d='M8 5.99988V3.99988C8 3.46944 8.21071 2.96074 8.58579 2.58566C8.96086 2.21059 9.46957 1.99988 10 1.99988H14C14.5304 1.99988 15.0391 2.21059 15.4142 2.58566C15.7893 2.96074 16 3.46944 16 3.99988V5.99988M19 5.99988V19.9999C19 20.5303 18.7893 21.039 18.4142 21.4141C18.0391 21.7892 17.5304 21.9999 17 21.9999H7C6.46957 21.9999 5.96086 21.7892 5.58579 21.4141C5.21071 21.039 5 20.5303 5 19.9999V5.99988H19Z'
+						stroke='#E73F5D'
+						strokeWidth='2'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					/>
+				</svg>
+
+				<div className='text-container'>
+					<h2>Excluir pergunta</h2>
+					<p>Tem certeza que você deseja excluir esta pergunta?</p>
+				</div>
+
+				<div className='buttons-container'>
+					<Button isGray onClick={() => setQuestionIdToBeDeleted('')}>
+						Cancelar
+					</Button>
+					<Button isDanger onClick={handleDeleteQuestion}>
+						Sim, excluir
+					</Button>
+				</div>
+			</Modal>
+
+			<Modal
+				contentLabel='Encerrar sala'
 				isOpen={showModal}
 				onRequestClose={() => setShowModal(false)}
-				preventScroll
 			>
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
-					width='40'
-					height='40'
-					viewBox='0 0 40 40'
+					viewBox='0 0 24 24'
 					fill='none'
 					stroke='currentColor'
-					strokeWidth='3'
+					strokeWidth='2'
 					strokeLinecap='round'
 					strokeLinejoin='round'
 				>
-					<circle cx='20' cy='20' r='16'></circle>
-					<line x1='25' y1='15' x2='15' y2='25'></line>
-					<line x1='15' y1='15' x2='25' y2='25'></line>
+					<circle cx='12' cy='12' r='10'></circle>
+					<line x1='15' y1='9' x2='9' y2='15'></line>
+					<line x1='9' y1='9' x2='15' y2='15'></line>
 				</svg>
 
 				<div className='text-container'>
